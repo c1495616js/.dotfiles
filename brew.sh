abdd64fc729b17bd6a5b5ea38c5b782d96ade3d6
Jerry Wang <jerryappleid761208@gmail.com>
2021-06-23 16:28:06 -0700

    Update brew.sh
    update command for installing brew

--- brew.sh
+++ brew.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install brew cask
 brew install cask
