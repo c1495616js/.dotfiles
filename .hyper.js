a7fdf1a3c32bd5ddbd3147606268677e9870c135
Jerry <jerryappleid761208@gmail.com>
2021-11-27 09:58:14 -0800

    add hyper

--- .hyper.js
+++ .hyper.js
@@ -1,133 +1,63 @@
-// Future versions of Hyper may add additional config options,
-// which will not automatically be merged into this file.
-// See https://hyper.is#cfg for all currently supported options.
-
 module.exports = {
   config: {
-    // Choose either "stable" for receiving highly polished,
-    // or "canary" for less polished but more frequent updates
-    updateChannel: 'canary',
-
-    // default font size in pixels for all tabs
-    fontSize: 12,
-
-    // font family with optional fallbacks
-    fontFamily: '"Fira Code", "Cousine for Powerline", "Inconsolata-dz for Powerline", monospace',
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    // cursorColor: 'rgba(248,28,229,0.8)',
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BLOCK',
-
-    // set to true for blinking cursor
-    cursorBlink: true,
-
-    // color of the text
-    foregroundColor: '#fff',
-
-    // terminal background color
-    backgroundColor: '#000',
-
-    // border color (window, tabs)
-    borderColor: '#333',
-
-    // custom css to embed in the main window
-    css: '',
-
-    // custom css to embed in the terminal window
-    termCSS: '',
-
-    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: true,
-
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '20px 24px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
+    updateChannel: "stable",
+    fontSize: 14,
+    fontFamily:
+      'Hack Nerd Font, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontWeight: "normal",
+    fontWeightBold: "bold",
+    lineHeight: 1,
+    letterSpacing: 0,
+    cursorColor: "rgba(248,28,229,0.8)",
+    cursorAccentColor: "#000",
+    cursorShape: "BLOCK",
+    cursorBlink: false,
+    foregroundColor: "#fff",
+    backgroundColor: "#000",
+    selectionColor: "rgba(248,28,229,0.3)",
+    borderColor: "#333",
+    css: "",
+    termCSS: "",
+    showHamburgerMenu: "",
+    showWindowControls: "",
+    padding: "12px 14px",
     colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
+      black: "#000000",
+      red: "#C51E14",
+      green: "#1DC121",
+      yellow: "#C7C329",
+      blue: "#0A2FC4",
+      magenta: "#C839C5",
+      cyan: "#20C5C6",
+      white: "#C7C7C7",
+      lightBlack: "#686868",
+      lightRed: "#FD6F6B",
+      lightGreen: "#67F86F",
+      lightYellow: "#FFFA72",
+      lightBlue: "#6A76FB",
+      lightMagenta: "#FD7CFC",
+      lightCyan: "#68FDFE",
+      lightWhite: "#FFFFFF",
     },
-
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // Powershell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
-    shellArgs: ['--login'],
-
-    // for environment variables
+    shell: "",
+    shellArgs: ["--login"],
     env: {},
-
-    // set to false for no bell
-    bell: 'SOUND',
-
-    // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false
-
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
+    bell: "SOUND",
+    copyOnSelect: false,
+    defaultSSHApp: true,
+    quickEdit: false,
+    macOptionSelectionMode: "vertical",
+    webGLRenderer: true,
   },
-
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
   plugins: [
-  	'hyper-chesterish',
-  	'hyper-tabs-enhanced',
-  	'hypercwd'
+    "hyper-tab-icons",
+    "hyper-search",
+    "hyperterm-paste",
+    "hypercwd",
+    "hyper-pane",
   ],
-
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
   localPlugins: [],
-
   keymaps: {
-    // Example
-    // 'window:devtools': 'cmd+alt+o',
-  }
+    "window:devtools": "cmd+alt+o",
+  },
 };
--- .zshrc
+++ .zshrc
@@ -271,3 +271,8 @@ export NVM_DIR="$HOME/.nvm"
 
 # Added by serverless binary installer
 export PATH="$HOME/.serverless/bin:$PATH"
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="/opt/homebrew/opt/node@14/bin:$PATH"
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
